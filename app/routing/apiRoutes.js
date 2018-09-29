2007df957b5a058b1afc839625bf4e4ea86cb9e9
Courtney Duggins <Marie1990@Courtneys-MacBook-Pro.local>
2018-09-29 10:03:11 -0400

    get and post routes

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,2 +1,24 @@
 var friendData = require("data/friend.js");
 
+
+module.exports = function (app){
+
+
+
+
+    app.get("api/friends.js", function(res, req){
+
+        res.json(friendData);
+
+    });
+
+    app.post("app/friends", function(res, req){
+
+
+
+
+    });
+};
+
+
+
